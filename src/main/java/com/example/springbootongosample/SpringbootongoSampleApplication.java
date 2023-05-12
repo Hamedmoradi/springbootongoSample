09fc7ebede4322af8037002fd0d32f710cb5fe07
HamedMoradi <hamedmoradi.mailbox@gmail.com>
2023-05-12 18:25:22 +0330

    Initial commit

---
+++ src/main/java/com/example/springbootongosample/SpringbootongoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootongosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootongoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootongoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootongosample/SpringbootongoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootongosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootongoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
